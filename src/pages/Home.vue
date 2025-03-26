49475efe763a85fa91392493bfa383d529275084
oleksandr <slimnation2@gmail.com>
2025-03-26 15:34:39 +0100

    test

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,16 @@
 <template>
     <div class="home">
-        <h1>All destinations</h1>
+        <h1 class="text-2xl font-semibold mb-5">All destinations</h1>
 
         <div class="destinations">
             <router-link
                 v-for="destination in destinations"
                 :key="destination.id"
-                :to="destination.slug"
+                :to="{ name: 'destination.show', params: { id: destination.id, slug: destination.slug } }"
             >
-            <h2>{{destination.name}}</h2>
-            <img :src="`/images/${destination.image}`" :alt="destination.slug"></router-link>
+                <h2>{{ destination.name }}</h2>
+                <img :src="`/images/${destination.image}`" :alt="destination.slug" class="rounded-md shadow-md" />
+            </router-link>
         </div>
     </div>
 </template>
@@ -20,7 +21,7 @@ import sourceData from "@/data.json";
 export default {
     data() {
         return {
-          destinations: sourceData.destinations,
+            destinations: sourceData.destinations,
         };
     },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/pages/Home.vue";
-import About from "@/pages/About.vue";
-import Brazil from "@/pages/Brazil.vue";
-import Hawaii from "@/pages/Hawaii.vue";
-import Jamaica from "@/pages/Jamaica.vue";
-import Panama from "@/pages/Panama.vue";
 
 const routes = [
     { path: "/", name: "Home", component: Home },
-    { path: "/about", name: "About", component: About },
-    { path: "/brazil", name: "brazil", component: Brazil },
-    { path: "/hawaii", name: "hawaii", component: Hawaii },
-    { path: "/jamaica", name: "jamaica", component: Jamaica },
-    { path: "/panama", name: "panama", component: Panama },
+    { path: "/destination/:id/:slug", name: "destination.show", component: () => import("@/pages/DestinationShow.vue") },
 ];
 
 const router = createRouter({
